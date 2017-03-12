5aa5eba3c8f7d4baca9adc4639df0447f9fcec85
awang <awang@marinsoftware.com>
2017-03-12 17:19:07 +0800

    dockerize

--- cron_job.sh
+++ cron_job.sh
@@ -4,4 +4,4 @@ echo "`date` running cron job"
 ## m h  dom mon dow   command
 #0  */3  * * *   bash /bbsparse/cron_job.sh
 
-python manage.py harvestlinks -c 30 > /bbsparse/logs/harvest.log 2>&1
+python manage.py harvestlinks -c 30 > /data/bbsparse/logs/harvest.log 2>&1
--- docker/entry_point.sh
+++ docker/entry_point.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo Starting bbsparse.
 
+service cron start
+
 cd /bbsparse
 python manage.py runserver 0.0.0.0:8001 > /data/bbsparse/logs/console.log 2>&1
